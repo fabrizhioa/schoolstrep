7287a688d612ab7cfe0cbe3b93b6f1c7a9f60b53
Braulio Vargas <vargas23.bv@gmail.com>
2022-11-23 22:42:50 -0600

    ShopPage template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<Home />} />
+      <Route path="/shop" element={<ShopPage />} />
     </Routes>
   );
 }
